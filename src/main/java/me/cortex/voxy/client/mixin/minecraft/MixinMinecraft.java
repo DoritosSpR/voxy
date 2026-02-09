4d91955943f7b3d355832e5dc0bd5e964464c737
Saejin Mahlau-Heinert <saejinmh@va.wagner.com>
2026-02-09 12:34:02 -0500

    Fix 1.21.1 Minecraft disconnect mixin targets

--- src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinMinecraft.java
+++ src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinMinecraft.java
@@ -10,7 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
-    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;ZZ)V", at = @At("TAIL"))
+    @Inject(method = {
+            "disconnect(Lnet/minecraft/client/gui/screens/Screen;Z)V",
+            "disconnect(Lnet/minecraft/client/gui/screens/Screen;)V"
+    }, at = @At("TAIL"), require = 0)
     private void voxy$injectWorldClose(CallbackInfo ci) {
         if (VoxyCommon.isAvailable() && VoxyClientInstance.isInGame) {
             VoxyCommon.shutdownInstance();
